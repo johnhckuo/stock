af8b0c247284215d2a8d688c8e124bd1997bd074
johnhckuo <johnhckuo@gmail.com>
2021-05-14 00:36:56 +0800

    adding postgres init script

---
+++ deploy/init.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS timetable;    
+
+CREATE DATABASE timetable;
+
+\c timetable;        
+
+CREATE TABLE Users (
+    ID bigint GENERATED ALWAYS AS IDENTITY,
+    Name character varying(50) NOT NULL,
+    PRIMARY KEY(ID)
+);
+
+CREATE TABLE Timeslots (
+    ID bigint GENERATED ALWAYS AS IDENTITY,
+    StartAt bigint NOT NULL,
+    EndAt bigint NOT NULL,
+    UserId bigint,
+    PRIMARY KEY(ID),
+    CONSTRAINT fk_id
+      FOREIGN KEY(UserId) 
+	  REFERENCES Users(ID)
+	  ON DELETE CASCADE
+);
+
